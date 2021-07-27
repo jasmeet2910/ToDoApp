eede94ee7800ede2488f57cf6010946d3b2c773c
jasmeet2910 <jasmeettuteja1993@gmail.com>
2021-07-27 23:30:33 +0530

    Added a delete list implementation

--- my-app/src/AddList.js
+++ my-app/src/AddList.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import logo   from './cross-svgrepo-com.svg'
+
 
 
 const AddList = () => {
@@ -19,13 +21,20 @@ const AddList = () => {
           setTextValue("")
         }
     }
+
+    const deleteList = (id) => {
+      const removedListItem = list.find((ls) => ls.id=== id)
+      //setList(list.filter((ls) => ls.id !== removedListItem.id))
+      let newList = list.filter(ls => ls.id !== removedListItem.id);
+      setList(newList);
+    }
     return (
         <>
           <input type="text"  id = "inputField" name= "name"  value={textValue} onChange={handleChange} onKeyPress={handleKeyDown} />
           {
               list.map((ls) => {
                 return (
-                    <div key={ls.id} style={{border:'2px solid red'}}>{ls.name}</div>
+                    <div key={ls.id} style={{border:'2px solid red', width:'148px', marginTop:'6px', color:'black'}}>{ls.name}<img src={logo} alt="svg Icon"  onClick={() => deleteList(ls.id)} style={{height:'20px',float:'right',marginLeft:'5px'}}/></div>
                 )
               })      
 }
